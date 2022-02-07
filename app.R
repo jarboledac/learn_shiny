32e987903766e40531984d5cf21dc23abc707f53
jarboledac <jefer2507@gmail.com>
2022-02-07 15:50:02 -0500

    avance en botones

--- app.R
+++ app.R
@@ -31,19 +31,67 @@ library(shiny)
 #  })
 #}
 
-ui<- fluidPage(
-  sliderInput("x", label = "If x is", min = 1, max = 50,
-                        value = 30),
-  sliderInput("y", label = "If y is", min = 1, max = 50,
-              value = 30),
-  "then x times 5 is",
-  textOutput("product")
+#ui<- fluidPage(
+#  sliderInput("x", label = "If x is", min = 1, max = 50,
+#                       value = 30),
+#  sliderInput("y", label = "If y is", min = 1, max = 50,
+#              value = 30),
+#  "then x times 5 is",
+#  textOutput("product")
+#)
+
+#server <- function(input, output, session){
+#    output$product <- renderText({
+#      input$x*input$y
+ #   })
+#}
+#
+
+
+# ui <- fluidPage(
+#   fileInput("file", NULL, accept = c(".csv", ".tsv")),
+#   numericInput("n", "Rows", value = 5, min = 1, step = 1),
+#   tableOutput("head"))
+#
+# server <- function(input, output, session){
+#   data <- reactive({
+#   req(input$file)
+#   ext <- tools::file_ext(input$file$name)
+#   switch(ext,
+#          csv = vroom::vroom(input$file$datapath, delim = ","),
+#          tsv = vroom::vroom(input$file$datapath, delim = "\t"),
+#          validate("Invalid file; Please upload a .csv or .tsv file")
+#   ) })
+# output$head <- renderTable({ head(data(), input$n)
+# }) }
+ui <- fluidPage(
+  fileInput("upload","Archivo",buttonLabel = "Upload...", multiple = TRUE, accept = c(".csv", ".tsv")),
+  tableOutput("files"),
+  downloadButton("report", "Generate report")
 )
-
 server <- function(input, output, session){
-    output$product <- renderText({
-      input$x*input$y
-    })
-}
-shinyApp(ui, server)
+  data <- reactive({
+    req(input$upload)
+    ext <- tools::file_ext(input$upload$name)
+    switch(ext,
+           csv = vroom::vroom(input$upload$datapath, delim = ","),
+           tsv = vroom::vroom(input$upload$datapath, delim = "\t"),
+           validate("Invalid file; Please upload a .csv or .tsv file"))
+           })
+  output$files <- renderTable(input$upload)
+  output$report <- downloadHandler(
+    filename = "report.html", content = function(file){
+      #params <- list(n = input$n)
+      id <- showNotification(
+        "Rendering report...",
+        duration = NULL,
+        closeButton = FALSE
+      )
+      on.exit(removeNotification(id), add = TRUE)
+      rmarkdown::render("sondeo_vif_opt.Rmd", output_file = file,
+                        #params = params,
+                        envir = new.env(parent = globalenv())
+      ) }
+  ) }
 
+shinyApp(ui, server)
