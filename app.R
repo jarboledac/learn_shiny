b45ca10699ed735f7067b0aa7fb97f67757ac65a
jarboledac <jefer2507@gmail.com>
2022-02-21 15:09:24 -0500

    conección drive

--- app.R
+++ app.R
@@ -1,78 +1,12 @@
 library(shiny)
 
-#ui <- fluidPage(
-#  "Hello World"
-#)
-
-#ui <- fluidPage(
-#  selectInput("dataset", label = "Dataset", choices = ls("package:datasets")),
-#  verbatimTextOutput("summary"),
- # tableOutput("table")
-#)
-
-#server<- function(input, output, session){
-#  dataset <- reactive(get(input$dataset, "package:datasets"))
-#  output$summary <- renderPrint({
-#    summary(dataset())
-#})
-#  output$table <- renderTable({
-#    dataset()
-#    })
-#}
-
-#ui <- fluidPage(
-#  textInput("name","what's your name?"),
-#  textOutput("greeting")
-#)
-
-#server <- function(input, output, session){
-#  output$greeting <- renderText({
-#    paste0("Hello ", input$name)
-#  })
-#}
-
-#ui<- fluidPage(
-#  sliderInput("x", label = "If x is", min = 1, max = 50,
-#                       value = 30),
-#  sliderInput("y", label = "If y is", min = 1, max = 50,
-#              value = 30),
-#  "then x times 5 is",
-#  textOutput("product")
-#)
-
-#server <- function(input, output, session){
-#    output$product <- renderText({
-#      input$x*input$y
- #   })
-#}
-#
+source("./drive.R")
 
 
-# ui <- fluidPage(
-#   fileInput("file", NULL, accept = c(".csv", ".tsv")),
-#   numericInput("n", "Rows", value = 5, min = 1, step = 1),
-#   tableOutput("head"))
-#
-# server <- function(input, output, session){
-#   data <- reactive({
-#   req(input$file)
-#   ext <- tools::file_ext(input$file$name)
-#   switch(ext,
-#          csv = vroom::vroom(input$file$datapath, delim = ","),
-#          tsv = vroom::vroom(input$file$datapath, delim = "\t"),
-#          validate("Invalid file; Please upload a .csv or .tsv file")
-#   ) })
-# output$head <- renderTable({ head(data(), input$n)
-# }) }
+
 ui <- fluidPage(
   mainPanel(img(src = "logo2.png", width = 1300),height = 140, width = 400),
   titlePanel(h1("Informe semanal", align = "center")),
-  fluidRow(column(4,br(), offset = 0),column(4,
-                  fileInput("upload","Archivos planos",buttonLabel = "Upload...",
-                            multiple = TRUE, accept = c(".csv", ".tsv"), width = 600),
-
-  )),
-  tableOutput("files"),
   fluidRow(column(4,br()),
            column(2,
                   dateInput("date1", label = h3("Fecha uno"), value = "2022-01-01"),
@@ -88,18 +22,13 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session){
-  data <- reactive({
-    req(input$upload)
-    ext <- tools::file_ext(input$upload$name)
-    switch(ext,
-           csv = vroom::vroom(input$upload$datapath, delim = ","),
-           tsv = vroom::vroom(input$upload$datapath, delim = "\t"),
-           validate("Invalid file; Please upload a .csv or .tsv file"))
-           })
-  output$files <- renderTable(input$upload)
   output$report <- downloadHandler(
     filename = "report.html", content = function(file){
-      #params <- list(n = input$n)
+      #id1 <- showNotification("Descargando archvio",duration = NULL,closeButton = FALSE,
+       #                       action = drive_download(file = "sondeo_de_opinion.xlsx", path = "./data/sondeo_de_opinion.xlsx"))
+      withProgress(drive_download(file = "sondeo_de_opinion.xlsx", path = "./data/sondeo_de_opinion.xlsx"),
+                   message = "Descargando archivo")
+      #on.exit(removeNotification(id1), add = TRUE)
       id <- showNotification(
         "Rendering report...",
         duration = NULL,
@@ -109,9 +38,15 @@ server <- function(input, output, session){
       rmarkdown::render("sondeo_vif_opt.Rmd", output_file = file,
                         #params = params,
                         envir = new.env(parent = globalenv())
-      ) })
-  #output$fecha1 <- renderPrint(input$date1)
-  #output$fecha2 <- renderPrint(input$date2)
+      )
+      fl <- "./data/sondeo_de_opinion.xlsx"
+      if(file.exists(fl)){
+        file.remove(fl)
+      }})
+  output$fecha1 <- renderPrint(input$date1)
+  output$fecha2 <- renderPrint(input$date2)
+
   }
+
 
 shinyApp(ui, server)
---
+++ drive.R
@@ -0,0 +1,18 @@
+library(googledrive)
+
+
+drive_auth(
+  email = "drivebackup.sic@gmail.com",
+  path = path,
+  scopes = "https://www.googleapis.com/auth/drive",
+  cache = gargle::gargle_oauth_cache(),
+  use_oob = gargle::gargle_oob_default(),
+  token = NULL
+)
+
+print(drive_ls(path = "Backup"))
+#drive_ls(path = NULL)
+
+#drive_download(file = "informe-diario-homicidio-20220220.pdf")
+#drive_rm("informe-diario-homicidio-20220220.pdf")
+#drive_rm("bla2")
